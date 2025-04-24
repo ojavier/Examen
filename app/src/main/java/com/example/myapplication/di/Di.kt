e19e9051c7266163ba2c24b19c05cc3ce23111af
ojavier <ojvilleda6@gmail.com>
2025-04-24 12:16:20 -0600

    Crear header personalizado

--- app/src/main/java/com/example/myapplication/di/Di.kt
+++ app/src/main/java/com/example/myapplication/di/Di.kt
@@ -1,2 +1,54 @@
 package com.example.myapplication.di
 
+import com.example.myapplication.data.remote.SudokuApiService
+import com.example.myapplication.data.remote.SudokuRemoteDataSource
+import com.example.myapplication.ApiKeyProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Di {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.api-ninjas.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val apiKey = ApiKeyProvider.API_KEY
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("X-Api-Key", apiKey)
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSudokuApiService(retrofit: Retrofit): SudokuApiService {
+        return retrofit.create(SudokuApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSudokuRemoteDataSource(api: SudokuApiService): SudokuRemoteDataSource {
+        return SudokuRemoteDataSource(api)
+    }
+}
